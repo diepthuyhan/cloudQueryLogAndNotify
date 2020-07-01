2d1c12e8cd319e7b5de220fc5ff41da40167a1d9
Ho Hoai Son <vlthson@mediba.jp>
2020-07-01 09:28:10 +0700

    dont add boto3 in requirements

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("readme.md", "r") as fh:
 REQUIRE = []
 with open("requirements.txt", "r") as f:
     requirements = f.readlines()
-    REQUIRE = [r.strip() for r in requirements]
+    REQUIRE = [r.strip() for r in requirements if not r.startwith("#")]
 
 
 setuptools.setup(
